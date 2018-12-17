40b018c3e1c7b770a3ee90a66d4f165a82f468ec
tuken <tuken24@icloud.com>
2018-12-17 21:30:21 +0900

    modify install-mysql.sh

--- Scripts/install-mysql-5.7.sh
+++ Scripts/install-mysql-5.7.sh
@@ -1,15 +1,10 @@
 #!/usr/bin/env bash
 
-sudo service mysql stop || echo "mysql not stopped"
-sudo stop mysql-5.6 || echo "mysql-5.6 not stopped"
 wget http://dev.mysql.com/get/mysql-apt-config_0.8.11-1_all.deb
-sudo debconf-set-selections <<< 'mysql-apt-config mysql-apt-config/select-tools select MySQL Server (Currently selected: mysql-5.7)'
-sudo debconf-set-selections <<< "mysql-apt-config mysql-apt-config/select-preview select mysql-5.7"
-sudo debconf-set-selections <<< "mysql-apt-config mysql-apt-config/select-server select mysql-5.7"
-sudo debconf-set-selections <<< "mysql-apt-config mysql-apt-config/select-product select Ok"
-DEBIAN_FRONTEND=noninteractive sudo dpkg -i mysql-apt-config_0.8.11-1_all.deb
+echo mysql-apt-config mysql-apt-config/select-server select mysql-5.7 | sudo debconf-set-selections
+sudo dpkg -i mysql-apt-config_0.8.11-1_all.deb
 sudo apt-get update -q
-sudo apt-get install -q -y mysql-server
+sudo apt-get install -q -y -o Dpkg::Options::=--force-confnew mysql-server
 sudo mysql_upgrade
 
 set -x
